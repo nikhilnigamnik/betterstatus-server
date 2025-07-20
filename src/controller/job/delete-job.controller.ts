1952d703516c4511c10f3b0fa9ee55d9f471e7cc
nikhilnigamnik <nikhilnigamnik@gmail.com>
2025-07-20 21:16:14 +0530

    Remove some controller and routes

--- src/controller/job/delete-job.controller.ts
+++
@@ -1,8 +0,0 @@
-import { jobService } from "@/services/job";
-import { Context } from "hono";
-
-export const deleteJobController = async (c: Context) => {
-  const { id } = c.req.param();
-  const job = await jobService.deleteJob(id);
-  return c.json(job);
-};
--- src/controller/job/get-job.controller.ts
+++
@@ -1,9 +0,0 @@
-import { jobService } from "@/services/job";
-import { Context } from "hono";
-
-export const getJobController = async (c: Context) => {
-  const { id } = c.req.param();
-
-  const job = await jobService.getJobById(id);
-  return c.json(job);
-};
